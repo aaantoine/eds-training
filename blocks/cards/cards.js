247b37b0fb00dbe23e9932a4bf9132356a86e9cf
antoine@adobe.com <antoine@adobe.com>
2024-09-25 17:01:02 +0200

    retour

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,29 +1,18 @@
-import { createOptimizedPicture, fetchPlaceholders } from '../../scripts/aem.js';
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default async function decorate(block) {
-  let placeholders = {};
-  try {
-    placeholders = await fetchPlaceholders('');
-  } catch (err) {
-    console.error('Error fetching placeholders:', err);
-  }
+export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  const newDiv = document.createElement('p');
-  newDiv.textContent = placeholders.clickHereForMore;
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) {
-      li.append(row.firstElementChild);
-    }
+    while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    li.append(newDiv);
     ul.append(li);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
 }
